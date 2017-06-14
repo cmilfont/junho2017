b8839a214efeb3f459d6a0bb10e5f80644adb4d6
Lucas Andrade <lucas.andradebenevides@gmail.com>
2017-06-14 17:15:11 -0300

    Identação

--- src/api/actions/brewery.js
+++ src/api/actions/brewery.js
@@ -1,36 +1,39 @@
 import { push } from 'react-router-redux';
 
 export const actions = {
-	add: 						'BREWERY_LIST_ADD',
-	edit: 					'BREWERY_LIST_EDIT',
-	remove:    			'BREWERY_LIST_REMOVE',
-	request: 			  'BREWERY_LIST_REQUEST',
-	requestSuccess: 'BREWERY_LIST_REQUEST_SUCCESS',
-}
+  add: 'BREWERY_LIST_ADD',
+  edit: 'BREWERY_LIST_EDIT',
+  remove: 'BREWERY_LIST_REMOVE',
+  request: 'BREWERY_LIST_REQUEST',
+  requestSuccess: 'BREWERY_LIST_REQUEST_SUCCESS',
+};
 
 export const mapStateToProps = ({ list }) => ({ list });
 
-export const mapDispatchToProps = (dispatch) => {
-	return {
-		request: () => dispatch({ type: actions.request }),
+export const mapDispatchToProps = dispatch => {
+  return {
+    request: () => dispatch({ type: actions.request }),
 
-		requestSuccess: payload	=> dispatch({
-			type: actions.requestSuccess,
-			payload,
-		}),
+    requestSuccess: payload =>
+      dispatch({
+        type: actions.requestSuccess,
+        payload,
+      }),
 
-		add: () => dispatch({ type: actions.add }),
+    add: () => dispatch({ type: actions.add }),
 
-		remove: payload => dispatch({
-			type: actions.remove,
-			payload,
-		}),
+    remove: payload =>
+      dispatch({
+        type: actions.remove,
+        payload,
+      }),
 
-		edit: payload => dispatch({
-			type: actions.edit,
-			payload,
-		}),
-	};
+    edit: payload =>
+      dispatch({
+        type: actions.edit,
+        payload,
+      }),
+  };
 };
 
 export default {};
